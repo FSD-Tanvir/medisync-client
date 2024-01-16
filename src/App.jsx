1991a4d2ab339b9bf33e00f40b2a81239a7e4fa3
tanvirfsd <tahasanpro@gmail.com>
2024-01-17 01:47:12 +0600

    start the project with created folder structure of homepage

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <h1 className="text-center">MediSync (Bridging the Gap between Medicines and Health Advice.)</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/home/faq/Faq.jsx
@@ -0,0 +1,11 @@
+
+
+const Faq = () => {
+    return (
+        <div>
+            Faq
+        </div>
+    );
+};
+
+export default Faq;
