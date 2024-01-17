e693a45c5246454cacfc0407a14cc9db99093edf
MD SAYEDUL HAQUE <125432652+Sayedul3731@users.noreply.github.com>
2024-01-17 15:01:46 +0600

    Banner & Advertise section added

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Home from './pages/home/Home.jsx'
+// import Home from './pages/home/Home.jsx'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div className='max-w-7xl mx-auto'>
-      <Home></Home>
+      <App></App>
     </div>
   </React.StrictMode>,
 )
