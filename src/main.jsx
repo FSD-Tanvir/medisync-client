120ade5b96ec2cc5b8ace01e6be1dde1a157b95a
tanvirfsd <tahasanpro@gmail.com>
2024-01-17 19:04:20 +0600

    site updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import './App.css'
-import Home from './pages/home/Home.jsx'
+import Home from './pages/home/Home'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div className='max-w-7xl mx-auto'>
-      <App></App>
+      <Home/>
     </div>
   </React.StrictMode>,
 )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,24 +1,27 @@
-
 import Footer from "../../components/shared/footer/Footer";
 import Articles from "./articles/Articles";
 import Products from "./products/Products";
 import Categories from "./categories/Categories";
 import Advertise from "./Advertise/Advertise";
 import Banner from "./banner/Banner";
-
+import Testimonials from "./testimonials/Testimonials";
+import Faq from "./faq/Faq";
 
 const Home = () => {
-    return (
-        <div>
-           <div className="flex min-h-[500px] mt-10 gap-10">
-                <Banner></Banner>
-                <Advertise></Advertise>
-            </div>
-           <Categories/>
-           <Products></Products>
-          <Articles></Articles>
-        </div>
-    );
+  return (
+    <div>
+      <div className="flex min-h-[500px] mt-10 gap-10">
+        <Banner></Banner>
+        <Advertise></Advertise>
+      </div>
+      <Categories />
+      <Products></Products>
+      <Articles></Articles>
+      <Testimonials />
+      <Faq />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
