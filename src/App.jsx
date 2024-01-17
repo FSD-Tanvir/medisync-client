21dbcd7383fec7453079588a9e40ee10ae2f52e2
RajMahammadAli <ali.rc.raj@gmail.com>
2024-01-17 13:33:54 +0600

    faq section added and designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import "./App.css";
+import Faq from "./pages/home/faq/Faq";
 function App() {
   return (
     <>
-      <h1 className="text-center">MediSync (Bridging the Gap between Medicines and Health Advice.)</h1>
+      <h1 className="text-center">
+        MediSync (Bridging the Gap between Medicines and Health Advice.)
+        <Faq />
+      </h1>
     </>
   );
 }
--- src/pages/home/faq/Faq.jsx
+++ src/pages/home/faq/Faq.jsx
@@ -1,11 +1,17 @@
-
+import FaqCard from "./FaqCard";
+import faqData from "./faqData";
 
 const Faq = () => {
-    return (
-        <div>
-            Faq
-        </div>
-    );
+  return (
+    <div className="w-full mt-20 p-8 bg-zinc-100">
+      <h1 className="text-2xl font-bold">Your Guide to Using Medisync</h1>
+      <div>
+        {faqData.map((fData, index) => (
+          <FaqCard key={fData.id} fData={fData} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Faq;
