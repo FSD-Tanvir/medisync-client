36e61e384fe9752392fc737c672db49086da5656
MD SAYEDUL HAQUE <125432652+Sayedul3731@users.noreply.github.com>
2024-02-04 23:27:43 +0600

    some customization added in the dashboard

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,17 +1,22 @@
 /* eslint-disable react/no-unescaped-entities */
-import { FaHome, } from "react-icons/fa";
 import { GiMeal } from "react-icons/gi";
 import { CgProfile } from "react-icons/cg";
-import { MdFoodBank, MdRateReview } from "react-icons/md";
+import { MdRateReview } from "react-icons/md";
 import { NavLink, Outlet, useLocation } from "react-router-dom";
-import { BiSolidFoodMenu } from "react-icons/bi";
+import { IoMdArrowDropright } from "react-icons/io";
 // import useAdmin from "../../hooks/useAdmin";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import doctorIcon from "../assets/DashboardIcons/images.png"
+import newsIcon from "../assets/DashboardIcons/news.png"
+import adviceIcon from "../assets/DashboardIcons/advice.png"
+import jobIcon from "../assets/DashboardIcons/jobs.png"
+import homeIcon from "../assets/DashboardIcons/home.webp"
 
 const Dashboard = () => {
+  const [open, setOpen] = useState(false)
   const isAdmin = " ";
   const location = useLocation();
-  const isDashboard =  location.pathname === "/dashboard" || false;
+  const isDashboard = location.pathname === "/dashboard" || false;
 
 
   useEffect(() => {
@@ -27,28 +32,40 @@ const Dashboard = () => {
   }, [location.pathname]);
 
   return (
-    <div className="flex flex-col max-w-7xl mx-auto md:flex-row-reverse">
-      <div className="w-full md:w-64 md:min-h-screen bg-[#478097] text-white">
-        <ul className="menu space-y-2">
+    <div className="flex flex-col  md:flex-row  border">
+      <div className={`w-full ${open ? "md:w-[40%] lg:w-[20%]" : "md:w-[10%] lg:w-[5%]"} duration-300 md:min-h-screen bg-[#f5f5f5] text-white pt-20 relative`}>
+        <div onClick={() => setOpen(!open)} className="md:-right-[14px] top-9 cursor-pointer w-[28px] h-[28px] rounded-full bg-blue-600 flex justify-center items-center absolute hidden md:flex">
+          <IoMdArrowDropright className={`${open && "rotate-180"}`} ></IoMdArrowDropright>
+        </div>
+        <ul className="menu space-y-2 text-black">
           {isAdmin ? (
             <>
               <li>
                 <NavLink
                   to="/Dashboard/doctors"
-                  className="font-semibold flex justify-start items-center gap-1 pl-5"
+                  className={({ isActive, isPending }) =>
+                    isActive
+                      ? "font-semibold flex justify-start items-center gap-1 pl-5 bg-[#3560fa] text-[#ffffff] w-[70%] mx-auto py-1"
+                      : isPending
+                        ? ""
+                        : "font-semibold flex justify-start items-center gap-1 pl-5 w-[70%] mx-auto py-1"}
                 >
-                  <MdFoodBank />
-                  All Doctors
+                  {open ? <span className="flex justify-center items-center gap-2"> <img src={doctorIcon} className="w-6 bg-white" alt="" />
+                    All Doctors</span> : <img src={doctorIcon} className="w-6 bg-white" alt="" />}
                 </NavLink>
               </li>
               <li>
                 <NavLink
-
                   to="/Dashboard/add-articles"
-
-                  className="font-semibold flex justify-start items-center gap-1 pl-5"
+                  className={({ isActive, isPending }) =>
+                    isActive
+                      ? "font-semibold flex justify-start items-center gap-1 pl-5 bg-[#3560fa] text-[#ffffff] w-[70%] mx-auto py-1"
+                      : isPending
+                        ? ""
+                        : "font-semibold flex justify-start items-center gap-1 pl-5 w-[70%] mx-auto py-1"}
                 >
-                  <BiSolidFoodMenu /> Add News & Articles
+                  {open ? <span className="flex justify-center items-center gap-2"> <img src={newsIcon} className="w-6 bg-white" alt="" />
+                    Add News & Articles</span> : <img src={newsIcon} className="w-6 bg-white" alt="" />}
                 </NavLink>
               </li>
               <li>
@@ -56,36 +73,44 @@ const Dashboard = () => {
 
                   to="/Dashboard/all-articles"
 
-                  className="font-semibold flex justify-start items-center gap-1 pl-5"
+                  className={({ isActive, isPending }) =>
+                    isActive
+                      ? "font-semibold flex justify-start items-center gap-1 pl-5 bg-[#3560fa] text-[#ffffff] w-[70%] mx-auto py-1"
+                      : isPending
+                        ? ""
+                        : "font-semibold flex justify-start items-center gap-1 pl-5 w-[70%] mx-auto py-1"}
                 >
-                  <BiSolidFoodMenu /> All News & Articles
+                  {open ? <span className="flex justify-center items-center gap-2"> <img src={newsIcon} className="w-6 bg-white" alt="" />
+                    All News & Articles</span> : <img src={newsIcon} className="w-6 bg-white" alt="" />}
                 </NavLink>
               </li>
               <li>
                 <NavLink
 
-                  to="/Dashboard/advices"
-                  className="font-semibold flex justify-start items-center gap-1 pl-5"
+                  to="/dashboard/advices/allAdvices"
+                  className={({ isActive, isPending }) =>
+                    isActive
+                      ? "font-semibold flex justify-start items-center gap-1 pl-5 bg-[#3560fa] text-[#ffffff] w-[70%] mx-auto py-1"
+                      : isPending
+                        ? ""
+                        : "font-semibold flex justify-start items-center gap-1 pl-5 w-[70%] mx-auto py-1"}
                 >
-                  <BiSolidFoodMenu />
-                  Advices
-
+                  {open ? <span className="flex justify-center items-center gap-2"> <img src={adviceIcon} className="w-6 bg-white" alt="" />
+                    Advices</span> : <img src={adviceIcon} className="w-6 bg-white" alt="" />}
                 </NavLink>
               </li>
-              {/* <li>
-                <NavLink
-                  to="/Dashboard/UpcomingMeals"
-                  className="font-semibold flex justify-start items-center gap-1 pl-5"
-                >
-                  <BiSolidFoodMenu /> Add Job
-                </NavLink>
-              </li> */}
               <li>
                 <NavLink
                   to="/Dashboard/job-panel"
-                  className="font-semibold flex justify-start items-center gap-1 pl-5"
+                  className={({ isActive, isPending }) =>
+                    isActive
+                      ? "font-semibold flex justify-start items-center gap-1 pl-5 bg-[#3560fa] text-[#ffffff] w-[70%] mx-auto py-1"
+                      : isPending
+                        ? ""
+                        : "font-semibold flex justify-start items-center gap-1 pl-5 w-[70%] mx-auto py-1"}
                 >
-                  <BiSolidFoodMenu /> All Jobs
+                  {open ? <span className="flex justify-center items-center gap-2"> <img src={jobIcon} className="w-6 bg-white" alt="" />
+                  All Jobs</span> : <img src={jobIcon} className="w-6 bg-white" alt="" />}
                 </NavLink>
               </li>
             </>
@@ -128,24 +153,25 @@ const Dashboard = () => {
         </ul>
         <div className="divider"></div>
         <>
-          <ul className="menu">
+          <ul className="menu text-black">
             <li>
               <NavLink
                 to="/"
-                className="font-semibold flex justify-start items-center gap-1 pl-5"
+                className="font-semibold flex justify-start items-center gap-1 pl-5 mt-5 w-[70%] mx-auto pb-5"
               >
-                <FaHome /> Home
+                {open ? <span className="flex justify-center items-center gap-2"> <img src={homeIcon} className="w-6" alt="" />
+                  Home</span> : <img src={homeIcon} className="w-6" alt="" />}
               </NavLink>
             </li>
           </ul>
         </>
       </div>
 
-      <div className="flex-1 overflow-auto">
-        {isDashboard? <><div className="flex items-center justify-center bg-blue-300 text-2xl lg:text-6xl font-bold min-h-screen text-center ">Welcome <br /> To  <br /> Admin Dashboard</div></> : <Outlet></Outlet>}
+      <div className="flex-1 overflow-auto md:w-[70%] lg:px-20">
+        {isDashboard ? <><div className="flex items-center justify-center bg-blue-300 text-2xl lg:text-6xl font-bold min-h-screen text-center ">Welcome <br /> To  <br /> Admin Dashboard</div></> : <Outlet></Outlet>}
       </div>
     </div>
-  );     
+  );
 
 };
 
