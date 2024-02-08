dc47e78adec43889da4148e09af909adacdfd245
Rasibul <rasibul179@gmail.com>
2024-02-08 19:19:57 +0600

    changes few design on userProduct caer

--- src/components/drawer/Drawer.jsx
+++ src/components/drawer/Drawer.jsx
@@ -1,13 +1,17 @@
-import UserProductCart from "../userProductCart/UserProductCart";
 
+import UserProductCart from "../userProductCart/UserProductCart";
+import { ImCross } from "react-icons/im";
 
 const Drawer = ({ isOpen, onClose }) => {
   return (
-    <div className={isOpen ? "fixed inset-0 pt-10 bg-gray-300 bg-opacity-30 z-50" : ""}>
-      <div className={`fixed inset-y-0 left-0 z-50 ${isOpen ? 'translate-x-0' : '-translate-x-full'} bg-white w-4/12  p-4 transition-transform ease-in-out duration-300`}>
+    <div className={isOpen ? "fixed inset-0 pt-10 bg-gray-300 bg-opacity-30  z-50" : ""}>
+      <div className={`fixed inset-y-0 left-0 z-50 ${isOpen ? 'translate-x-0' : '-translate-x-full'} bg-white w-full md:w-2/3 lg:w-1/3 p-4 transition-transform ease-in-out duration-300`}>
+        <div className="flex justify-between items-center">
+        <button className="text-gray-500" onClick={onClose}><ImCross /></button>
+        <button onClick={onClose}>Back to Home</button>
+        </div>
         {/* Drawer content goes here */}
-        <UserProductCart></UserProductCart>
-        <button className="text-gray-500" onClick={onClose}>Close Drawer</button>
+        <UserProductCart />
       </div>
     </div>
   );
