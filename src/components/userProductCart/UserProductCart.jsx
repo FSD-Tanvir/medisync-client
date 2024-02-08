0628ccbf9b7b6e139ef165872ef18ed3df5de3d0
Rasibul <rasibul179@gmail.com>
2024-02-08 18:58:28 +0600

    delete method implement on user product cart

--- src/components/userProductCart/UserProductCart.jsx
+++ src/components/userProductCart/UserProductCart.jsx
@@ -1,19 +1,34 @@
+import useAxiosPublic from "../../hooks/useAxiosPublic";
 import useProductCart from "../../hooks/useProductCart";
 
 
 const UserProductCart = () => {
-    try {
-        const [productCart, isLoading, refetch] = useProductCart();
 
-        if (isLoading) {
-            return <div>Loading...</div>;
+
+    const [productCart, isLoading, refetch] = useProductCart();
+    const axiosPublic = useAxiosPublic()
+
+
+    const handleDeleteProduct = async (productId) => {
+        try {
+            const res = await axiosPublic.delete(`/productCart/${productId}`);
+            // Product deleted successfully, refetch the productCart data
+            refetch();
+        } catch (error) {
+            console.error("Error deleting product:", error);
+            // Handle error, show a message, or take appropriate action
         }
-        // console.log(productCart)
-        if (!productCart || !productCart.length) {
-            return <div>No product data available.</div>;
-        }
-        return (
-            <div className="flex flex-col">
+    };
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+    // console.log(productCart)
+    if (!productCart || !productCart.length) {
+        return <div>No product data available.</div>;
+    }
+    return (
+        <div className="flex flex-col">
             {productCart.map((product, index) => (
                 <div key={index} className=" p-4 mb-4 shadow-md rounded-md">
                     <div className="flex items-center">
@@ -25,7 +40,7 @@ const UserProductCart = () => {
                     </div>
                     <div className="flex justify-between mt-2">
                         <p className="text-gray-700"><span className="font-bold">Price:</span> ${product.price} /piece</p>
-                        <button className="text-red-500">Delete</button>
+                        <button onClick={() => handleDeleteProduct(product._id)} className="text-red-500">Delete </button>
                     </div>
                     <div className="mt-2">
                         <p className="text-gray-700"><span className="font-bold">Total Price:</span> ${product.totalPrice} <span className="font-bold">Quintity:</span>{product.quintity}</p>
@@ -33,11 +48,9 @@ const UserProductCart = () => {
                 </div>
             ))}
         </div>
-        );
-    } catch (error) {
-        console.error("Error in UserProductCart:", error);
-        return <div>Error loading product data.</div>;
-    }
-};
+    );
+
+}
+
 
 export default UserProductCart;
