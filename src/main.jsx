2bf29b2a1fa85e3f94f2e1885dad0b2cc0454bd5
MD SAYEDUL HAQUE <125432652+Sayedul3731@users.noreply.github.com>
2024-02-14 22:04:00 +0600

    translation method added as a litle

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./App.css";
+import "./i18n.js"
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routes.jsx";
 import { Toaster } from "react-hot-toast";
@@ -12,17 +13,19 @@ import AuthProvider from "./Porviders/AuthProvider.jsx";
 
 import io from "socket.io-client";
 
-const socket = io.connect("http://localhost:5001");
-
+const socket = io.connect("http://localhost:5000");
+console.log(socket);
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <RouterProvider router={router} />
-        <Toaster />
-      </AuthProvider>
-    </QueryClientProvider>
+    <React.Suspense fallback="loading...">
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <RouterProvider router={router} />
+          <Toaster />
+        </AuthProvider>
+      </QueryClientProvider>
+    </React.Suspense>
   </React.StrictMode>
 );
