14da9f968a67db0457fc71b41886c8dee62d64b3
MD SAYEDUL HAQUE <125432652+Sayedul3731@users.noreply.github.com>
2024-02-19 21:03:02 +0600

    emailJS implemented in contact us page

--- src/pages/contactUs/ContactUs.jsx
+++ src/pages/contactUs/ContactUs.jsx
@@ -1,26 +1,40 @@
 import toast from "react-hot-toast";
 import { FaEnvelope, FaLocationDot, FaPhone } from "react-icons/fa6";
 import BannerSimple from "../../components/shared/Banners/BannerSimple/BannerSimple";
-import { useEffect } from "react";
+import { useEffect, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const ContactUs = () => {
+  
+  const form = useRef();
 
   useEffect(()=>{
     window.scroll(0,0)
   },[])
 
-  const handleSubmit = (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
-    // const form = e.target;
-    toast.success("Thank you for contacting us");
-    e.target.reset();
+
+    emailjs
+      .sendForm('service_5kh83xc', 'template_le7xmvf', form.current, {
+        publicKey: 'P2LmmYYgwMmNEKEx5',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          toast("Thank you for your contacting us")
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
   };
 
   return (
     <div className="min-h-screen ">
       {/* contact banner section  */}
-      
-      <BannerSimple imgUrl="https://i.ibb.co/JRMRpvD/3117.jpg" text1="Contact Us"/>
+
+      <BannerSimple imgUrl="https://i.ibb.co/JRMRpvD/3117.jpg" text1="Contact Us" />
       {/* contact main content */}
       <div className="mt-16">
         <h2 className="text-2xl font-semibold text-center">
@@ -37,15 +51,15 @@ const ContactUs = () => {
             </p>
             <div className="flex flex-col gap-2 pl-5 lg:pl-0">
               <p className="flex gap-2 flex-wrap items-center text-black/70 text-lg font-medium">
-                <FaEnvelope size={20} className="text-cyan-400" />{" "}
+                <FaEnvelope size={20} className="text-blue-500" />{" "}
                 mdhasanali9139@gmail.com
               </p>
               <p className="flex gap-2 items-center text-lg text-black/70 font-medium">
-                <FaPhone size={20} className="text-cyan-400" /> +8801512345678,
+                <FaPhone size={20} className="text-blue-500" /> +8801512345678,
                 +8801512345678
               </p>
               <p className="flex gap-2 items-center text-lg text-black/70 font-medium">
-                <FaLocationDot size={20} className="text-cyan-400" /> This is a
+                <FaLocationDot size={20} className="text-blue-500" /> This is a
                 dummy address -- Mirpur 1, Block-A - 1216.
               </p>
             </div>
@@ -53,47 +67,15 @@ const ContactUs = () => {
           {/* form  */}
           <div className="flex-1">
             <h2 className="text-xl font-semibold text-center lg:text-start">Send Message</h2>
-            <form onSubmit={handleSubmit}>
-              <div className="flex flex-col justify-center items-center lg:justify-start lg:items-start gap-2 mt-2">
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Name"
-                  required
-                  className="border border-cyan-400 w-3/4  focus:outline-cyan-400 focus:outline px-3 py-2 "
-                />
-                <input
-                  type="text"
-                  name="subject"
-                  placeholder="Subject"
-                  required
-                  className="border border-cyan-400 w-3/4 focus:outline-cyan-400 focus:outline px-3 py-2 "
-                />
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  required
-                  className="border border-cyan-400 w-3/4 focus:outline-cyan-400 focus:outline px-3 py-2"
-                />
-
-                <textarea
-                  name="message"
-                  placeholder="message"
-                  required
-                  className="border border-cyan-400 w-3/4 focus:outline-cyan-400 focus:outline px-3 py-2 "
-                ></textarea>
-                <div className="w-full lg:w-3/4
-                mt-4">
-                  <button
-                    type="submit"
-                    className="px-10 ml-32 lg:ml-0 lg:px-20 bg-[#003049] py-2 rounded-none uppercase hover:bg-[#003049] hover:border-none transition ease-in duration-300"
-                  >
-                    Submit
-                  </button>
-                </div>
-              </div>
-            </form>
+            <div className="px-1">
+              <form ref={form} onSubmit={sendEmail}>
+                <input type="text" placeholder="Your Name" className="border border-blue-500 w-full lg:w-3/4 focus:outline-blue-500 focus:outline px-3 py-2 my-5" name="user_name" />
+                <input type="email" placeholder="Your Email" className="border border-blue-500 w-full lg:w-3/4 focus:outline-blue-500 focus:outline px-3 py-2 " name="user_email" />
+                <textarea name="message" placeholder="Write here your message" className="border border-blue-500 w-full lg:w-3/4 focus:outline-blue-500 focus:outline px-3 py-2 my-5" />
+                <input type="submit"
+                  className="border capitalize rounded-md hover:border-hover-border-color hover:text-hover-text-color font-semibold py-2 shadow-[-2px_-2px_12px_2px_rgba(0,0,0,0.1),_2px_2px_12px_2px_rgba(0,0,0,0.1)] bg-button-bg-color text-white hover:bg-button-bg-hover-color transition duration-[250] ease-linear w-full lg:w-3/4 " value="Send" />
+              </form>
+            </div>
           </div>
         </div>
       </div>
