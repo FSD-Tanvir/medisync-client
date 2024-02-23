913d50b991fa49530bbac1d2aed01becdebe02fc
mdhasanali39 <contact.hasankhanyt@gmail.com>
2024-02-23 21:29:56 +0600

    I have added some card dynamic and static on admin overview page on dashboard

---
+++ src/pages/Dashboard/adminOverview/charts/appointmentChart/AppointmentChart.jsx
@@ -0,0 +1,13 @@
+const AppointmentChart = () => {
+
+  return (
+    <>
+      {/* appointments */}
+      <div className="border rounded-lg h-[20vh] w-full p-4 shadow-lg col-span-3">
+        <h3 className="text-lg font-semibold mb-4">Appointments</h3>
+      </div>
+    </>
+  );
+};
+
+export default AppointmentChart;
