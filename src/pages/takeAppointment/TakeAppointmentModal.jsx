3ff8a9e55f2f3ece52f5ff23716ff0fb126ad71e
mdhasanali39 <contact.hasankhanyt@gmail.com>
2024-02-28 07:40:02 +0600

    I have updated take appointment page that can when user selected date and timeslot they will be see an modal for input their phone number and one next button when they click on next button they able to click book appointment button when click the book appointment button their application will be succeeded and data saved on database, if they are already booked this timeslot and date they cant't booked again and already booked date will be disabled

---
+++ src/pages/takeAppointment/TakeAppointmentModal.jsx
@@ -0,0 +1,55 @@
+import Button from "../../components/shared/button/Button";
+
+const TakeAppointmentModal = ({setExtraInfoOfUser, setIsModal}) => {
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const mobile_number = e.target?.mobile_number?.value;
+    if(mobile_number){
+      setExtraInfoOfUser(mobile_number)
+      setIsModal(false)
+    }
+    // console.log(mobile_number);
+  };
+
+  return (
+    <div className="fixed inset-0 z-[100] bg-primary-bg-color/40 flex justify-center items-center">
+      <div>
+        <form onSubmit={handleSubmit} className={`p-4 lg:p-8 relative w-full sm:w-auto my-6 mx-auto max-w-3xl shadow-lg border bg-blue-500/35 rounded-lg`}>
+          {/* <h1 className="backdrop-blur-sm text-2xl lg:text-4xl whitespace-nowrap w-min mb-8 border-b-4 border-b-white capitalize">
+            add job
+          </h1> */}
+          <div className="grid gap-5 grid-cols-1 md:grid-cols-2 justify-center items-baseline mb-3">
+            {/* user mobile number  */}
+            <div className="space-y-5 drop-shadow-md w-full">
+              <label
+                htmlFor="mobile_number"
+                className="block text-white border-l-2 border-white font-semibold pl-2"
+              >
+                Mobile Number
+              </label>
+              <input
+                id="mobile_number"
+                type="number"
+                name="mobile_number"
+                required
+                placeholder="Enter Your Mobile Number"
+                className="p-3 block w-full outline-1 border valid:outline-blue-500 rounded-md invalid:outline-red-600"
+              />
+            </div>
+          </div>
+          {/* next button  */}
+          <Button
+            btnName="Next"
+            classForButton="px-2 w-1/3"
+            btnType="submit"
+            classForDiv="text-center mt-8"
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default TakeAppointmentModal;
